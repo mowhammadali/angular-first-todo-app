c02665eb1b40808d02c8131766c1ea49aa6735c4
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-05-29 21:44:17 +0330

    [implement] row-1 of todo generator section

---
+++ src/app/components/todo-generator/todo-generator.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-todo-generator',
+    templateUrl: './todo-generator.component.html',
+    styleUrls: ['./todo-generator.component.css'],
+})
+export class TodoGeneratorComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
