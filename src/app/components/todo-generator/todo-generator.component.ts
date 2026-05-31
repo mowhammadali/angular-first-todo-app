bfb526598f8376dd048ccb934c1a72c4945b9381
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-05-31 19:41:08 +0330

    [add] new todo to list

--- src/app/components/todo-generator/todo-generator.component.ts
+++ src/app/components/todo-generator/todo-generator.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { type TodoType } from '../../pages/home/home.component';
 
 @Component({
     selector: 'app-todo-generator',
@@ -6,7 +7,40 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./todo-generator.component.css'],
 })
 export class TodoGeneratorComponent implements OnInit {
+    @ViewChild('todoTitle') title: ElementRef;
+    @ViewChild('todoDescription') description: ElementRef;
+    @ViewChild('todoPriority') priority: ElementRef;
+    @ViewChild('todoStatus') status: ElementRef;
+
+    @Output('addNewTodo') addTodo = new EventEmitter<TodoType>();
+
     constructor() {}
+
+    public addNewTodo(): void {
+        const {
+            title: { nativeElement: titleElement },
+            description: { nativeElement: descriptionElement },
+            priority: { nativeElement: priorityElement },
+            status: { nativeElement: statusElement },
+        } = this;
+
+        const inputFillCheck =
+            titleElement.value.trim() &&
+            descriptionElement.value.trim() &&
+            priorityElement.value.trim() &&
+            statusElement.value.trim();
+
+        if (inputFillCheck) {
+            const newTodo: TodoType = {
+                title: titleElement.value,
+                description: descriptionElement.value,
+                priority: priorityElement.value,
+                status: statusElement.value,
+            };
+
+            this.addTodo.emit(newTodo);
+        }
+    }
 
     ngOnInit(): void {}
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export type TodoType = {
+    title: string;
+    description: string;
+    status: string;
+    priority: string;
+    [key: string]: any;
+};
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+    public todos: TodoType[] = [];
+
     constructor() {}
+
+    public addNewTodo(todo: TodoType) {
+        this.todos = [...this.todos, todo];
+    }
 
     ngOnInit(): void {}
 }
